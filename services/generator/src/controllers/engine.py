8b93364dd5d2e4bdb0f4ad2c79b76dfa3997ab14
sebjwallace <sebjwallace@gmail.com>
2019-10-05 12:26:32 +0100

    built generator

--- services/generator/src/controllers/engine.py
+++ services/generator/src/controllers/engine.py
@@ -1,5 +1,8 @@
-from db import connect, cursor
+import math
 import pandas as pd
+import numpy as np
+import scipy.spatial
+from db import connect, cursor
 
 def generate(transaction):
   customerId = transaction['customerId']
@@ -8,14 +11,30 @@ def generate(transaction):
 
   updateCustomerProduct(customerId, productId, quantity)
 
-  query = cursor()
-  query.execute('SELECT * FROM customerProduct')
-  print(query.fetchall(), flush=True)
-
   data = pd.read_sql_query('SELECT * FROM customerProduct', connect())
   customerProductMatrix = data.pivot_table(index='customerid', columns='productid', values='quantity', fill_value=0)
 
-  print(customerProductMatrix, flush=True)
+  customerIndex = customerProductMatrix.index.get_loc(customerId)
+  customerVector = np.asarray([customerProductMatrix.loc[customerId]])
+
+  distances = scipy.spatial.distance.cdist(customerProductMatrix, customerVector, metric='euclidean')
+  distances[customerIndex][0] = math.inf
+  
+  customerProductMatrix['Distances'] = distances
+  customerProductMatrix = customerProductMatrix.sort_values('Distances')
+  del customerProductMatrix['Distances']
+
+  bestMatchingCustomers = customerProductMatrix.head()
+  bestMatchingCustomerBinaries = np.where(bestMatchingCustomers > 0, 1, 0)
+  consensus = np.expand_dims(np.sum(bestMatchingCustomerBinaries, axis=0), 0)
+  consensus[customerVector > 0] = 0
+
+  customerProductMatrix.loc['Rank'] = np.squeeze(consensus, 0)
+  customerProductMatrix = customerProductMatrix.T.sort_values('Rank', ascending=False)
+
+  recommendedProducts = customerProductMatrix[customerProductMatrix['Rank'] > 0]
+
+  return recommendedProducts
 
 
 def updateCustomerProduct(customerId, productId, quantity):
